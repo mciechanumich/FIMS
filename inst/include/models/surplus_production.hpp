ea9cbcb5d720943a5a6b352a8a2b0105934c7538
Matthew Supernaw <matthew.supernaw@noaa.gov>
2025-02-20 08:56:45 -0500

    Update surplus_production.hpp

--- inst/include/models/surplus_production.hpp
+++ inst/include/models/surplus_production.hpp
@@ -1,16 +1,24 @@
 #ifndef FIMS_MODELS_SURPLUS_PRODUCTION_HPP
 #define FIMS_MODELS_SURPLUS_PRODUCTION_HPP
 
-
+#include <set>
 #include "fishery_model_base.hpp"
 
 namespace fims_popdy {
 
     template<typename Type>
     class SurplusProduction : public FisheryModelBase<Type> {
+        
+        std::set<uint32_t> population_ids;
+        
     public:
 
         virtual void Evaluate() {
+        }
+        
+        
+        void AddPopulation(uint32_t id){
+            this->population_ids.insert(id);
         }
     };
 
